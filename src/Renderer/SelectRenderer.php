8697f9ddaf64c7f23cd41a9b992976b4521243e8
Doğu Abaris <abaris@null.net>
2024-11-20 23:32:07 +0100

    SelectRenderer: Add new property to select data
    — Added `selectedOption` to specify the currently selected option.
    
    These changes extend the data passed to the Mustache template.
    
    Change-Id: I61b80bb7f19c95539d71705d94076138844b0e00

--- src/Renderer/SelectRenderer.php
+++ src/Renderer/SelectRenderer.php
@@ -87,6 +87,7 @@ class SelectRenderer implements IRenderer {
 		$selectData = [
 			'id' => $this->sanitizer->sanitizeText( $component->getId() ),
 			'isDisabled' => $component->isDisabled(),
+			'selectedOption' => $component->getSelectedOption(),
 			'attributes' => $this->resolve( $this->sanitizer->sanitizeAttributes( $component->getAttributes() ) ),
 			'options' => $this->prepareOptions( $component ),
 			'optGroups' => $this->prepareOptGroups( $component ),
