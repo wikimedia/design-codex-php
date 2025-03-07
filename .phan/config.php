6128e5702f51fb92879f2ec24418bb3a7f76e51d
Doğu Abaris <abaris@null.net>
2025-03-07 01:00:18 +0100

    phan: Simplify configuration
    Change-Id: I5339a3f52141de25d4a353a5d0fcaa518d3b8e0d

--- .phan/config.php
+++ .phan/config.php
@@ -2,24 +2,6 @@
 
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config-library.php';
 
-// Parse only a subset of dependencies
-$cfg['directory_list'] = array_diff( $cfg['directory_list'], [ 'vendor/' ] );
-
-$cfg['directory_list'] = array_merge(
-	$cfg['directory_list'],
-	[
-		'tests',
-		'vendor/krinkle/intuition',
-		'vendor/psr/log',
-		'vendor/psr/http-message',
-		'vendor/guzzlehttp/psr7',
-		'vendor/zordius/lightncandy',
-		'vendor/phpunit/phpunit',
-		'vendor/wikimedia/services',
-		'.phan/stubs',
-	]
-);
-
 $cfg['strict_method_checking'] = true;
 $cfg['strict_object_checking'] = true;
 $cfg['strict_property_checking'] = true;
