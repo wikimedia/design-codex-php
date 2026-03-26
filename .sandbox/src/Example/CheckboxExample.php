7f56ed9df506268fa7bfc3b5643922070ca87a44
Eileen Blessing <kabiblessing5@gmail.com>
2026-03-26 20:51:25 +0200

    sandbox: Add the checked, disabled and inline Checkboxes
    Bug: T418302
    Change-Id: I4a4270e93727846346356f12dfdac45f196089da

--- .sandbox/src/Example/CheckboxExample.php
+++ .sandbox/src/Example/CheckboxExample.php
@@ -10,12 +10,12 @@ class CheckboxExample {
 	 * @return string
 	 */
 	public static function create( Codex $codex ): string {
-		return $codex->Checkbox(
+		$defaultCheckbox = $codex->Checkbox(
 			inputId: 'password-reset-checkbox',
 			label: $codex->Label(
 				labelText: 'Send password reset emails only when both email address and username are provided.',
 				description: 'This improves privacy and helps prevent unsolicited emails.',
-				descriptionId: 'password-reset-checkbox-description'
+				descriptionId: 'password-reset-checkbox-description',
 			),
 			name: 'password-reset',
 			value: '1',
@@ -25,7 +25,39 @@ class CheckboxExample {
 			inputAttributes: [
 				'class' => 'bar',
 				'data-toggle' => 'checkbox-option',
-			]
+			],
 		);
+
+		// Checked checkbox, ticked by default.
+		$checkedCheckbox = $codex->Checkbox(
+			inputId: 'checked-checkbox',
+			label: $codex->Label(
+				labelText: 'This checkbox is checked by default.',
+			),
+			checked: true,
+		);
+
+		// Disabled checkbox.
+		$disabledCheckbox = $codex->Checkbox(
+			inputId: 'disabled-checkbox',
+			label: $codex->Label(
+				labelText: 'This checkbox is disabled.',
+			),
+			disabled: true,
+		);
+
+		// Inline checkbox.
+		$inlineCheckbox = $codex->Checkbox(
+			inputId: 'inline-checkbox',
+			label: $codex->Label(
+				labelText: 'This checkbox is inline.',
+			),
+			inline: true,
+		);
+
+		return $defaultCheckbox .
+			$checkedCheckbox .
+			$disabledCheckbox .
+			$inlineCheckbox;
 	}
 }
